c2e98a3b7076e4339a2aa11b198a3b7be8b9376a
Haloxx <80098945+xvhHaloxx@users.noreply.github.com>
2023-01-01 03:56:19 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-// Dependancies
 const express = require('express');
 const bodyParser = require('body-parser')
 
-// Engine path
-const enginePath = "stockfish_15.1_win_x64_avx2/stockfish-windows-2022-x86-64-avx2.exe";
+const enginePath = "stockfish-latest/stockfish.exe";
 
-// Setup server
 const app = express();
 
-// Parse data into body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Setup chess engine
 const Engine = require('node-uci').Engine;
 var engine = new Engine(enginePath);
 
@@ -20,8 +15,8 @@ async function setup() {
   engine = new Engine(enginePath);
   await engine.init();
 
-  await engine.setoption('Hash', (1024 * 2)); // This will use 2 GB of RAM
-  await engine.setoption('Threads', 4); // Logical processors of CPU
+  await engine.setoption('Hash', (1024 * 2));
+  await engine.setoption('Threads', 2);
 
 
   await engine.setoption('UCI_Elo', 3800),
@@ -34,16 +29,13 @@ async function setup() {
 (async () => {
   setup();
 
-  // Routes
   app.get('/api/solve', async (req, res) => {
     console.log(`Ran for ${req.query.fen}`);
     try {
       await engine.position(req.query.fen);
       
-      // Set the board to fen string provided and solve it
       const result = await engine.go({ depth: 20 });
 
-      // Send back the results
       res.send(result.bestmove);
 	  console.log(`Best Move: ${result.bestmove}`)
     } catch (error) {
@@ -53,6 +45,5 @@ async function setup() {
     }
   });
 
-  // Start listening
   app.listen(3000);
 })()
