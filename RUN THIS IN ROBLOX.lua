a773dde1e7f4e423405fcb3b21ad8d850f839625
Haloxx <80098945+xvhHaloxx@users.noreply.github.com>
2023-01-01 16:16:48 +1100

    Update RUN THIS IN ROBLOX.lua

--- RUN THIS IN ROBLOX.lua
+++ RUN THIS IN ROBLOX.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/xvhHaloxx/Roblox-Scripts/chess-ai/script.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/xvhHaloxx/Roblox-Scripts/Chess-AI-Updated/script.lua"))()
